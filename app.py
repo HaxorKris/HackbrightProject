c8752d031860b05763e765abbf12bf378fb882ba
Kris Weltz <kris@squirrel.local>
2014-03-24 17:21:12 -0700

    first push

---
+++ app.py
@@ -0,0 +1,141 @@
+from flask import Flask, render_template, redirect, request, flash, url_for, session
+import model
+
+app = Flask(__name__)
+app.secret_key = "thisisnotmuchofasecret"
+
+
+##################################################################
+
+
+@app.route("/")
+def index():
+	userid = session.get("userid")
+	if session.get("userid"):
+		return redirect(url_for("home"))
+	else:
+		return render_template("index.html")
+	# else:   // swap this code back in to resume testing against index.html
+	# 	fbID = '6019968'  # Rodie's ID #
+	# 	return render_template("index.html", fbID=fbID)
+
+
+@app.route("/check-fbid", methods=["POST"])
+def check_for_user():
+	fbid = request.form['fbid'] #from the post, collect fbid
+	session['fbid'] = fbid	
+	print "try-lookup"
+	try: 
+		user = model.session.query(model.User).filter_by(facebookId=fbid).one()
+		session['userid'] = user.id
+		print "%r" % user
+	except:
+		return url_for("register")
+	return url_for("home")
+
+
+@app.route("/register")
+def register():
+	print "fbid %r" % session['fbid']
+	return render_template("register.html")
+
+    
+@app.route("/register", methods=["POST"])
+def submit_registration():
+	FBID = session.get("fbid")
+	emailform = request.form.get("alt_email")
+	ageform = request.form.get("age")
+	cityform = request.form.get("city")
+	stateform = request.form.get("state")
+	zipcodeform = request.form.get("zipcode")
+	model.register_user(FBID, emailform, ageform, cityform, stateform, zipcodeform)
+
+	# user = session.query(User).filter_by(facebookId=FBID).one()  
+	# session['userid'] = user.id
+	
+	return redirect(url_for("index"))
+
+# TODO: look up - window.location="http://.../" + response.authResponse.userID
+
+@app.route("/clear_session")
+def logout():
+    session.clear()
+    return redirect(url_for("index"))
+
+
+@app.route("/home")
+def home():
+	userid = session.get("userid")
+	recent_posts = model.recent_posts(userid)
+#	recent_assists = model.recent_assists(uid)
+#	recent_replies = model.recent_replies(uid)
+	return render_template("home.html", recent_posts=recent_posts)
+
+
+# @app.route("/messages")
+# def inbox():
+# 	return render_template("messages.html")
+
+
+# @app.route("/profile")
+# def profile():
+# 	return render_template("your_profile.html")
+
+
+# @app.route("/user/<uid>")
+# def userpage():
+# 	return render_template("user.html")
+
+
+@app.route("/posts/<postid>")
+def post_details(postid):
+	post = model.getPostInfo(postid)
+	comments = model.getCommentsForPost(postid)
+
+	return render_template("post.html", postid=postid, post=post, comments=comments)
+
+@app.route("/submit")
+def post():
+
+	return render_template("create_post.html")
+
+@app.route("/submit", methods=["POST"])
+def submission():
+	FBID = session.get("fbid")
+	userid = session.get("userid")
+
+
+	roleform = request.form.get("role")
+	shortsummaryform = request.form.get("shortSummary")
+	longdescriptionform = request.form.get("longDescription")
+	zipcodeform = request.form.get("zipcode")
+	isasapform = request.form.get("isASAP")
+	canweekdaysform = request.form.get("canWeekdays")
+	caneveningsform = request.form.get("canEvenings")
+	canweekendsform = request.form.get("canWeekends")
+	cantravelform = request.form.get("canTravel")
+	canmeetform = request.form.get("canMeet")
+	buslinesform = request.form.get("busLines")                
+
+	model.submit_post(userid, roleform, shortsummaryform, longdescriptionform, zipcodeform, isasapform, 
+		canweekdaysform, caneveningsform, canweekendsform, cantravelform, canmeetform, buslinesform)
+
+	return redirect(url_for("home"))
+
+	# get postID
+	# return /posts/ + postID
+
+# @app.route("/history")
+# def history():
+# 	return render_template("history.html")
+
+
+# @app.route("/search")
+# def search():
+# 	return render_template("search.html")
+
+#TODO: route for /backdoor
+
+
+if __name__ == "__main__":
+    app.run(debug = True)
---
+++ model.py
@@ -0,0 +1,137 @@
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String
+from sqlalchemy import ForeignKey
+# from sqlalchemy import *
+from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import sessionmaker, scoped_session
+from sqlalchemy.orm.exc import NoResultFound
+import time
+import datetime
+import sqlite3
+
+##############################################################################
+
+ 
+ENGINE = create_engine("sqlite:///test.db", echo=False)
+session = scoped_session(sessionmaker(bind=ENGINE, autocommit = False, autoflush = False))
+
+Base = declarative_base()
+Base.query = session.query_property()
+
+
+##############################################################################
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key = True)
+    facebookId = Column(Integer)
+    altEmail = Column(String(64), nullable=True)
+    age = Column(Integer)
+    city = Column(String(64), nullable=True)
+    state = Column(String(2)) # get this to a 2-letter value from a drop-down
+    zipcode = Column(String(15))
+    registrationDate = Column(Integer)
+
+
+class Post(Base):
+	__tablename__ = "listings"
+	id = Column(Integer, primary_key = True)
+	ownerId = Column(Integer, ForeignKey('users.id'))
+	timeStamp = Column(Integer)
+	zipcode = Column(String(15), nullable=True)
+	role = Column(String(16))
+	shortSummary = Column(String(30))
+	longDescription = Column(String(300))
+	pictureId = Column(Integer)
+	isASAP = Column(Integer)
+	canWeekdays = Column(Integer)
+	canEvenings = Column(Integer)
+	canWeekends = Column(Integer)
+	canTravel = Column(Integer)
+	canMeet = Column(Integer)
+	busLines = Column(String(64), nullable=True)  # two splits - ",", whitespace? or regex both
+	offerExtendedTo = Column(Integer)
+
+	author = relationship("User", backref=backref("listings", order_by=id))
+
+
+class Response(Base):
+	__tablename__ = "responses"
+	id = Column(Integer, primary_key = True)
+	authorId = Column(Integer, ForeignKey('users.id'))
+	listingId = Column(Integer, ForeignKey('listings.id'))
+	role = Column(String(16))
+	timeStamp = Column(Integer) 
+	zipcode = Column(String(15), nullable=True)
+	comment = Column(String(300))
+	canASAP = Column(Integer) 
+	canWeekdays = Column(Integer)
+	canEvenings = Column(Integer)
+	canWeekends = Column(Integer)
+	canTravel = Column(Integer)
+	canMeet = Column(Integer)
+	busLines = Column(String(64), nullable=True)
+	wasOffered = Column(Integer)
+	wasOfferedAssist = Column(Integer)
+	offerStatus = Column(String(64))
+	offerAssistStatus = Column(String(64))
+	timeSelected = Column(Integer)
+	lastUpdated = Column(Integer)
+
+	responder = relationship("User", backref=backref("responses"), order_by=id)
+	post = relationship("Post", backref=backref("responses"), order_by = id)
+
+
+##############################################################################
+
+# metadata = MetaData()
+# metadata.create_all(ENGINE)
+
+
+def register_user(fbId, emailform, ageform, cityform, stateform, zipcodeform):
+	temp_user = User(facebookId=fbId, altEmail=emailform, age=ageform, city=cityform, state=stateform, 
+				zipcode=zipcodeform, registrationDate=int(time.time())) #check time.time usage
+	session.add(temp_user)
+	session.commit()
+
+def submit_post(userid, roleform, shortsummaryform, longdescriptionform, zipcodeform, isasapform, 
+	canweekdaysform, caneveningsform, canweekendsform, cantravelform, canmeetform, buslinesform):
+
+	temp_post = Post(ownerId=userid, role=roleform, shortSummary=shortsummaryform, 
+		longDescription=longdescriptionform, zipcode=zipcodeform, isASAP=isasapform, 
+		canWeekdays=canweekdaysform, canEvenings=caneveningsform, canWeekends=canweekendsform, 
+		canTravel=cantravelform, canMeet=canmeetform, busLines=buslinesform, timeStamp=int(time.time()))
+	session.add(temp_post)
+	session.commit()
+
+def recent_posts(uid):
+	u = session.query(User).get(uid)
+	post_list = session.query(Post).filter_by(ownerId=u.id)
+	return post_list
+
+
+def recent_assists(uid):
+	u = session.query(User).get(uid)
+	assist_list = session.query(Request).filter_by(authorId=u.id)
+	return assist_list
+
+
+def recent_replies(uid):
+	u = session.query(User).get(uid)
+	reply_list = session.query(Assist).filter_by(authorId=u.id)
+	return reply_list
+
+def getPostInfo(postid):
+	post = session.query(Post).get(postid)
+	return post
+
+def getCommentsForPost(postid):
+	c = session.query(Post).get(postid)
+	comments = session.query(Response).filter_by(listingId=c.id)
+	return comments
+
+
+
+
